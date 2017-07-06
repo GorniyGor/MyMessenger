2675401264dd9ef668e20a44fe7c7b812ee8beae
Egor Mikhalchenko <gorniygor@gmail.com>
2017-07-06 15:31:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gor/messenger/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.gor.messenger;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.menu_drawer_layout);*/
+
+        /*Toolbar mActionBarEnterPhone = (Toolbar) findViewById(R);*/
+
+    }
+}
